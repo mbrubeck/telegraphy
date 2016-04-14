df6f706ba05ee1336a1b7b01016b51e4c9b79707
lpelleau <loic.pelleau@insa-rennes.fr>
2016-04-14 18:57:44 -0500

    Slim added to project

--- telegraphy.rb
+++ telegraphy.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 require 'sinatra'
+require 'slim'
 require 'rugged'
 include Rugged
 
@@ -29,13 +30,13 @@ get %r{/(.*)} do |path|
   end
 
   if path.empty?
-    erb :index
+    slim :index
   else
     file = @tree.find { |f| f[:name] == path }
     redirect "/" if file.nil?
 
     @f = $repo.lookup(file[:oid])
-    erb :file
+    slim :edit
   end
 end
 
@@ -68,121 +69,3 @@ post %r{/(.*)} do |path|
 
   redirect "/#{path}"
 end
-
-__END__
-
-@@ index
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
-    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
-
-    <title>Telegraphy</title>
-  </head>
-  <body>
-    <nav>
-      <div class="nav-wrapper green lighten-1">
-        <a href="/" class="brand-logo">&nbsp;Telegraphy</a>
-        <ul class="right">
-          <li>
-            <a class="waves-effect waves-light modal-trigger" href="#newfile">
-              <i class="material-icons">add</i>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-
-    <div class="container">
-      <div id="newfile" class="modal bottom-sheet">
-        <form method="post">
-          <div class="modal-content">
-            <h4 class="green-text text-lignten-1">New file on repository</h4>
-            <div class="input-field">
-              <input name="name" id="name" type="text" class="validate">
-              <label for="name">New file name</label>
-            </div>
-          </div>
-          <div class="modal-footer">
-            <button id="nameBtn" type="submit" disabled class="disabled modal-action modal-close waves-effect waves-light btn green lighten-1 white-text">Create<i class="material-icons right">send</i></button>
-          </div>
-        </form>
-      </div>
-
-      <div class="collection"><% @tree.each {|o| %>
-        <a href="<%= File.join('/',@path,o[:name]) %>" class="collection-item">
-          <%= o[:name] %>
-        </a>
-        <% } %>
-      </div>
-    </div>
-    
-    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
-    <script>
-      $(document).ready(function() {
-        $('.modal-trigger').leanModal();
-        $('#name').on('input', function() {
-          console.log($('#name').val());
-          if ($(this).val() == "") {
-            if (!$('#nameBtn').hasClass('disabled')) {
-              $('#nameBtn').addClass('disabled');
-              $('#nameBtn').attr('disabled', '');
-            }
-          } else {
-            $('#nameBtn').removeClass('disabled');
-            $('#nameBtn').removeAttr('disabled');
-          }
-        });
-      });
-    </script>
-  </body>
-</html>
-
-@@ file
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
-    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
-
-    <title><%= @path %> - Telegraphy</title>
-  </head>
-  <body>
-    <nav>
-      <div class="nav-wrapper green lighten-1">
-        <a href="/" class="brand-logo">&nbsp;Telegraphy</a>
-      </div>
-    </nav>
-
-    <div class="container">
-      <div class="row">
-        <div class="col s12">
-          <div class="card hoverable">
-            <form method="post">
-              <div class="card-content">
-                <span class="card-title"><%= @path %></span>
-                <div class="input-field">
-                  <textarea id="data" name="data" class="materialize-textarea"><%= @f.content %></textarea>
-                  <label for="data">Content here</label>
-                </div>
-              </div>
-              <div class="card-action">
-                <button class="btn waves-effect waves-light green lighten-1" type="submit">Save<i class="material-icons right">send</i></button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-    
-    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
-  </body>
-</html>
-
